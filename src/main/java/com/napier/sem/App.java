d3aca87bd937405e08fadd7c466e71d4b357b983
Lin Thura <40516599@live.napier.ac.uk>
2022-12-23 10:16:00 +0630

    integration testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -19,8 +19,8 @@ public class App {
 
 
         //All the countries in the world organised by largest population to smallest.
-        //ArrayList<Country> cou = app.getCountryWorld();
-        //app.displayCountry(cou);
+        ArrayList<Country> cou = app.getCountryWorld();
+        app.displayCountry(cou);
         //All the cities in the world organised by largest population to smallest.
        // ArrayList<City> cty = app.getCityWorld();
         //app.displayCity(cty);
@@ -46,7 +46,8 @@ public class App {
             String strSelect =
                     "SELECT Code, Name, Continent, Region, Population, Capital "
                             + "FROM country"
-                            + "WHERE Region = 'Central Africa' ";
+                            + "WHERE Region = 'Central Africa' "
+                            + "ORDER BY Population DESC";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
 
