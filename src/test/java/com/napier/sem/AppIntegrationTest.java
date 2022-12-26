2fc8cc457c3efa50ca656139e365b5883434ae63
Hanzarniwin40527436 <101241512+Hanzarniwin40527436@users.noreply.github.com>
2022-12-26 15:14:33 +0630

    Integration test with database

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.nio.charset.CoderResult;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,44 +23,28 @@ public class AppIntegrationTest
 
     @Test
     void testprintcountryTest() {
-        ArrayList<Country> cou = new ArrayList<Country>();
-        Country couty=new Country();
-        couty.setCode("ABW");
-        couty.setName("Aru-ba");
-        couty.setContinent("North America");
-        couty.setRegion("Caribbean");
-        cou.add(couty);
-        cou=app.getCountryWorld();
-        assertEquals(couty.getCode(),"ABW");
-        assertEquals(couty.getName(),"Aru-ba");
-        assertEquals(couty.getContinent(),"North America");
-        assertEquals(couty.getRegion(),"Caribbean");
+        ArrayList<Country> cou=app.getCountryWorld();
+        Country cty= cou.get(0);
+        assertEquals(cty.getCode(),"CHN");
+        assertEquals(cty.getName(),"China");
+        assertEquals(cty.getContinent(),"Asia");
+        assertEquals(cty.getRegion(),"Eastern Asia");
     }
     @Test
     void testgetcity() {
-        ArrayList<City> cty = new ArrayList<>();
-        City city = new City();
-        city.setID(23232);
-        city.setName("Seoul");
-        city.setDistrict("Seoul");
-        city.setPopulation(9981619);
-        cty.add(city);
-        cty=app.getCityWorld();
-        assertEquals(city.getID(),23232);
-        assertEquals(city.getName(),"Seoul");
-        assertEquals(city.getPopulation(),9981619);
+        ArrayList<City> cty = app.getCityWorld();
+        City city = cty.get(0);
+        assertEquals(city.getID(),1024);
+        assertEquals(city.getName(),"Mumbai (Bombay)");
+        assertEquals(city.getDistrict(),"Maharashtra");
+        assertEquals(city.getPopulation(),10500000);
     }
     @Test
     void testprintcapitalcitiesTest() {
-        ArrayList<City> capty = new ArrayList<City>();
-        City capt=new City();
-        capt.setName("Jarcata");
-        capt.setPopulation(1231233);
-        capt.setCountryCode("Dubai");
-        capty.add(capt);
-       capty=app.getcapitalcitiesintheworld();
-        assertEquals(capt.getName(),"Jarcata");
-        assertEquals(capt.getPopulation(),1231233);
-        assertEquals(capt.getCountryCode(),"Dubai");
+        ArrayList<City> capty = app.getcapitalcitiesintheworld();
+        City capt=capty.get(0);
+        assertEquals(capt.getName(),"Seoul");
+        assertEquals(capt.getPopulation(),9981619 );
+        assertEquals(capt.getCountryCode(),"South Korea");
     }
 }
